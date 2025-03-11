7ced5a235c9033c10446754056b36a080881275e
Sergty Meshchankin <sergomes@gmail.com>
2025-03-11 22:23:30 +0500

    done task 7

--- 7_exception2.py
+++ 7_exception2.py
@@ -13,12 +13,26 @@
     
 """
 
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+
+def discounted(price, discount, max_discount=20):
+    try:
+        price = float(price)
+        discount = float(discount)
+        max_discount = int(max_discount)
+        price = abs(price)
+        discount = abs(discount)
+        max_discount = abs(max_discount)
+        if max_discount > 101:
+            raise ValueError('Максимальная скидка не должна быть больше 100')
+        if discount > max_discount:
+            price_with_discount = price
+        else:
+            price_with_discount = price - (price * discount / 100)
+        return price_with_discount
+    except (TypeError, ValueError):
+        print('некорректное значение или тип')
+
+
 if __name__ == "__main__":
     print(discounted(100, 2))
     print(discounted(100, "3"))
